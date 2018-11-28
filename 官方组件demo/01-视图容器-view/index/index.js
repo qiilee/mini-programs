5fc412ea1d1cc01c2ba632c06276900775479c9d
qilei <qiilee@126.com>
2018-11-28 14:11:02 +0800

    New: 官方组件demo下 练习

--- 官方组件demo/01-视图容器-view/index/index.js
+++ 官方组件demo/01-视图容器-view/index/index.js
@@ -1,9 +1,31 @@
-const app = getApp()
-
+var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data: {
-
+    toView: 'red',
+    scrollTop: 100
   },
-  onLoad: function () {
+  upper: function(e) {
+    console.log(e)
   },
+  lower: function(e) {
+    console.log(e)
+  },
+  scroll: function(e) {
+    console.log(e)
+  },
+  tap: function(e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function(e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  }
 })
