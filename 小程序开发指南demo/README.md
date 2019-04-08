83c520aed2e69f8814169814ed0760c8db54a22a
caoming <381679709@qq.com>
2019-04-08 15:47:48 +0800

    test: 小程序开发指南demo

---
+++ 小程序开发指南demo/README.md
@@ -0,0 +1,2 @@
+对应[小程序开发指南](https://developers.weixin.qq.com/ebook?action=get_post_info&docid=0008aeea9a8978ab0086a685851c0a)
+
---
+++ 小程序开发指南demo/pages/index/index.js
@@ -0,0 +1,22 @@
+Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        time: (new Date()).toString(),
+        w: 'w',
+        W: 'W',
+        var2: undefined,
+        var3: null,
+        var4: "var4",
+        a: 1,
+        b: 2,
+        c: 3,
+        length: 6,
+        array: [{
+            message: 'foo',
+        }, {
+            message: 'bar'
+        }]
+    },
+})
